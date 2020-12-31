bf031930fd1b780acf64ba8b22f9db3058b1340f
samsuddin <samsuddin@eatlbd.com>
2020-12-31 15:38:56 +0600

    service auth and hystris

---
+++ service-hystrixdashboard/src/test/java/com/hystrix/servicehystrixdashboard/ServiceHystrixdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hystrix.servicehystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHystrixdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
